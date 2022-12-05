4067f59d4831ac5e0c4a379ea1c760d3677c6541
Benoit Gacher <benoitgacher@gmail.com>
2022-12-05 17:14:18 +0100

    burger menu full css

--- src/components/Nav/nav.jsx
+++ src/components/Nav/nav.jsx
@@ -2,12 +2,16 @@
 import './nav.scss';
 
 // Import NPM
-import React from 'react';
+import React, { useState } from 'react';
 
 // Import img
 import icon from '../../assets/icon.png';
+import burgerButton from '../../assets/burger-menu-icon.png';
 
 function Nav() {
+  // Variable d'état
+  /*  const [open, setOpen] = useState('close'); */
+
   return (
     <div className="nav">
       <a href="home" className="navbar--logo">
@@ -15,12 +19,18 @@ function Nav() {
       </a>
       <nav className="navbar">
         <div className="navbar--link">
+          <input className="checkbox" type="checkbox" name="" id="" />
           <ul className="navbar--link__list">
-            <li><a href="home">Home</a></li>
-            <li><a href="whoarewe">Who are we ?</a></li>
-            <li><a href="cocktails">Cocktails</a></li>
-            <li><a href="tools">Tools & Tips</a></li>
+            <li className="navbar--link__list__element"><a className="navbar--link__list__element__button" href="home">Home</a></li>
+            <li className="navbar--link__list__element"><a className="navbar--link__list__element__button" href="whoarewe">Who are we ?</a></li>
+            <li className="navbar--link__list__element"><a className="navbar--link__list__element__button" href="cocktails">Cocktails</a></li>
+            <li className="navbar--link__list__element"><a className="navbar--link__list__element__button" href="tools">Tools & Tips</a></li>
           </ul>
+          <div className="hamburger-lines">
+            <span className="line line1" />
+            <span className="line line2" />
+            <span className="line line3" />
+          </div>
         </div>
       </nav>
     </div>
