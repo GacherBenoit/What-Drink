1f2077873e6278e93854992e3fa8160ae81b29e1
Benoit Gacher <benoitgacher@gmail.com>
2023-01-25 14:08:46 +0100

    video integration

--- src/components/Main/main.jsx
+++ src/components/Main/main.jsx
@@ -4,10 +4,13 @@ import './main.scss';
 // Import NPM
 import React from 'react';
 
+// components
+import VideoShow from '../VideoShow/videoShow';
+
 function Main() {
   return (
     <div className="main">
-      <h3>Main</h3>
+      <VideoShow />
     </div>
   );
 }
---
+++ src/components/VideoShow/videoShow.jsx
@@ -0,0 +1,18 @@
+// Import CSS
+import './videoShow.scss'
+
+// Import NPM
+import React from 'react';
+
+// Import video
+import video from '../../assets/video/video-01.mp4';
+
+function VideoShow() {
+  return (
+    <div className="video--container">
+      <video className="video--cocktail" autoPlay loop muted id="video " src={video} />
+    </div>
+  );
+}
+
+export default VideoShow;
