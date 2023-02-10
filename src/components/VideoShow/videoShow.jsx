f02746a3c3f949c698b7e859a108e22299694041
Benoit Gacher <benoitgacher@gmail.com>
2023-02-10 14:35:27 +0100

    scroll ok

--- src/components/VideoShow/videoShow.jsx
+++ src/components/VideoShow/videoShow.jsx
@@ -3,18 +3,18 @@ import './videoShow.scss';
 
 // Import NPM
 import React from 'react';
+import { HashLink } from 'react-router-hash-link';
 
 // Import video
 import video from '../../assets/video/video-01.mp4';// Import NPM
-
-// Component
-import LinkButton from '../LinkButton/linkButton';
 
 function VideoShow() {
   return (
     <div className="video--container">
       <video className="video--cocktail" autoPlay loop muted id="video " src={video} />
-      <LinkButton link="#welcome" buttonClassname="welcomeButton" linkClassname="welcomeLink" labelName="Welcome" />
+      <button type="button" className="welcomeButton">
+        <HashLink smooth to="/#welcome">Welcome</HashLink>
+      </button>
     </div>
   );
 }
