511ec13c3d1833ba2bb6de1be3a298f01caf7f87
Benoit Gacher <benoitgacher@gmail.com>
2023-02-10 10:44:24 +0100

    components added for routing

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,20 +3,24 @@ import './App.scss';
 import '../../styles/index.scss';
 // Import NPM
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { Routes , Route } from 'react-router-dom';
 
 // Import Components
 import Nav from '../Nav/nav';
 import Main from '../Main/main';
 import Footer from '../Footer/footer';
+import ToolsAndTips from '../ToolsAndTips/toolsAndTips';
+import WhoWeAre from '../WhoWeAre/whoWeAre';
 
 function App() {
   return (
     <div className="app">
       <Nav />
-      <BrowserRouter>
-        <Main />
-      </BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/tools&tips" element={<ToolsAndTips />} />
+        <Route path="/whoWeAre" element={<WhoWeAre />} />
+      </Routes>
       <Footer />
     </div>
   );
---
+++ src/components/WhoWeAre/whoWeAre.jsx
@@ -0,0 +1,13 @@
+// import CSS
+import './whoWeAre.scss';
+
+// Import NPM
+import React from 'react';
+
+function WhoWeAre() {
+  return (
+    <h1>Tools and Tips </h1>
+  );
+}
+
+export default WhoWeAre;
