32b3e01222d0c7dc7841b21ddf16b550672afc05
Benoit Gacher <benoitgacher@gmail.com>
2023-02-23 11:56:25 +0100

    switch class with usstate ok

--- src/components/Nav/nav.jsx
+++ src/components/Nav/nav.jsx
@@ -13,20 +13,21 @@ import logo from '../../assets/images/Logo-01.png';
 import Search from '../Search/search';
 
 function Nav() {
+  const [toggle, setToggle] = useState(true);
   return (
     <div className="navbar">
       <nav>
         <div className="navbar--burger__menu">
-          <input className="navbar--burger__menu__checkbox" type="checkbox" name="" id="" />
+          <button className="navbar--burger__menu__button" type='button' onClick={()=> setToggle(!toggle)} />
           <div className="navbar--burger__menu__lines">
-            <span className="navbar--burger__menu__lines__one" />
-            <span className="navbar--burger__menu__lines__two" />
-            <span className="navbar--burger__menu__lines__three" />
+            <span className={toggle ? 'navbar--burger__menu__lines__one' : 'navbar--burger__menu__lines__one__open'} />
+            <span className={toggle ? 'navbar--burger__menu__lines__two' : 'navbar--burger__menu__lines__two__open'} />
+            <span className={toggle ? 'navbar--burger__menu__lines__three' : 'navbar--burger__menu__lines__three__open'} />
           </div>
           <div className="navbar--burger__menu__logo">
             <img className="navbar--burger__menu__logo__item" src={logo} alt="" />
           </div>
-          <div className="navbar--burger__menu__items">
+          <div className={toggle ? 'navbar--burger__menu__items' : 'navbar--burger__menu__items__open'}>
             <li className="navbar--burger__menu__link "><NavLink className="navbar--burger__menu__link__button" to="/">Home</NavLink></li>
             <li className="navbar--burger__menu__link"><NavLink className="navbar--burger__menu__link__button" to="tools&tips">Tools and tip</NavLink></li>
             <li className="navbar--burger__menu__link"><NavLink className="navbar--burger__menu__link__button" to="/whoWeAre">Who are we ?</NavLink></li>
