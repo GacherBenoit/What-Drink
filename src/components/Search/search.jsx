e12d04e3f29da76045f75b2ecead22451c295af7
Benoit Gacher <benoitgacher@gmail.com>
2023-02-27 11:49:03 +0100

    function map ok

--- src/components/Search/search.jsx
+++ src/components/Search/search.jsx
@@ -8,17 +8,18 @@ import axios from 'axios';
 // Import image
 import glass from '../../assets/images/glass.png';
 import icon from '../../assets/images/icon.png';
+
 // Base URL for search by name
 const baseURL = 'https:/www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita';
 
 function Search() {
   const [search, SetSearch] = useState('');
-  const [recipes, setReciptes] = useState([]);
+  const [recipes, setRecipes] = useState([]);
 
   useEffect(() => {
     axios.get(baseURL).then((response) => {
-      console.log(response.data);
-      setReciptes(response.data);
+     console.log(response.data);
+      setRecipes(response.data.drinks);
     })
       .catch((err) => {console.log(err)});
   }, []);
@@ -37,7 +38,7 @@ function Search() {
             <img className="navbar--search__input__items__image" src={glass} alt="" />
           </button>
           <ul>
-      
+            { recipes && recipes.map((recipe) => <li key={recipe.idDrink}>{recipe.strDrink}</li>)}
           </ul>
         </div>
       </div>
