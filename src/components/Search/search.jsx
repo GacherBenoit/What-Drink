c43f9a58f4ee9cbee2ec5aa0caedad497df1c283
Benoit Gacher <benoitgacher@gmail.com>
2023-03-14 12:06:54 +0100

    commentary has added

--- src/components/Search/search.jsx
+++ src/components/Search/search.jsx
@@ -8,14 +8,13 @@ import axios from 'axios';
 // Import image
 import glass from '../../assets/images/glass.png';
 import icon from '../../assets/images/icon.png';
-import { element } from 'prop-types';
 
 function Search() {
   const [search, SetSearch] = useState('');
   const [recipes, setRecipes] = useState([]);
-  const navigationPosition = { current: -1 }; 
-  // Important to start at -1. We need a position equal to the index of the table.The properties will be increment at every key push.
-  // Here ,we start at -1 and the first incrementation qwe will start at 0 , the first entry of the table.
+  const navigationPosition = { current: -1 };
+  // We create on object to set the propertie of position at every interraction
+  // The properties will be increment at every key push.
 
   // Base URL for search by cocktail category because
   // we use free access and cant have the full list for the momment
@@ -38,40 +37,57 @@ function Search() {
   };
 
   const keyboardNavigation = (event) => {
-    if (search && search) {
-      const listElement = document.getElementsByClassName('navbar--search__input__items__list__element'); // Je crée un tableau avec tte les classes
-      const elementToRemove = document.getElementsByClassName('highLight');
+    if (search && search) { // We check if the input is'nt empty
+      const listElement = document.getElementsByClassName('navbar--search__input__items__list__element'); // We create a table with all propositions
+      const elementToRemove = document.getElementsByClassName('highLight'); // We create a table with the recipes with highlight className
 
+      // On arrowdown interraction, if the current position is set at the end of the table:
+      // We set the currentposition at -1 ( because it will be finally set at 0 with incrementation)
+      // We remove the previous highlight className (the preview element will disapear)
       if (event.key === 'ArrowDown') {
         if (navigationPosition.current === listElement.length - 1) {
           navigationPosition.current = -1;
           elementToRemove[0].classList.remove('highLight');
           console.log(navigationPosition.current);
         }
+
+        // At every interraction with arrowDown, the position is increment by +1
+        // We add the className highlight to the index of the table with the current position.
+        // We add the className highlight to the index of the table with the current position.
         navigationPosition.current += 1;
         listElement[navigationPosition.current].classList.add('highLight');
         console.log(navigationPosition.current);
 
+        // If we have two element with the classname highlight , we remove the previous one
+        // (here at the index 0 cause to the different direction navigation )
         if (elementToRemove.length === 2) {
           elementToRemove[0].classList.remove('highLight');
         }
       }
+
+      // If arrowUp is the first interraction, the current position is set at -1
+      // So we set the current position to the end of the proposition's table
       if (event.key === 'ArrowUp') {
         if (navigationPosition.current === -1) {
           navigationPosition.current = listElement.length;
-          console.log(listElement);
         }
+
+        // If the current position is set to 0 (at the begining of the list)
+        // We set the current posittion to the end of the proposition's table
+        // And remove the previous element with className highlight
         if (navigationPosition.current === 0) {
           navigationPosition.current = listElement.length;
           elementToRemove[0].classList.remove('highLight');
-          console.log(listElement);
         }
 
-        console.log(navigationPosition.current);
+        // At every arrowUp interraction , we decrement the current position by -1
+        // We add the className highlight to the index of the table with the current position.
         navigationPosition.current -= 1;
         listElement[navigationPosition.current].classList.add('highLight');
         console.log(navigationPosition.current);
 
+        // If we have two element with the classname highlight , we remove the previous one
+        // (here at the index 1 cause to the different direction navigation )
         if (elementToRemove.length === 2) {
           elementToRemove[1].classList.remove('highLight');
         }
