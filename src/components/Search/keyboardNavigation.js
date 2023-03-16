e101ec17420fdff6fbc5085ea86f54040ba8f91c
Benoit Gacher <benoitgacher@gmail.com>
2023-03-16 10:56:04 +0100

    function for navigation in other file

---
+++ src/components/Search/keyboardNavigation.js
@@ -0,0 +1,74 @@
+// We create on object to set the propertie of position at every interraction
+// The properties will be increment at every key push.
+const navigationPosition = { current: -1 };
+
+export default function keyboardNavigation(
+  evt, // The event object to have the key properties to check what keyboard touch has pressed
+  search, // useState for read the controlled field
+  SetSearch, // useState setter for controlled field
+  currentProposition, // the current proposition stored in useRef Hook
+) {
+  if (search && search) { // We check if the input is'nt empty
+    const listElement = document.getElementsByClassName('navbar--search__input__items__list__element'); // We create a table with all propositions
+    const elementToRemove = document.getElementsByClassName('highLight'); // We create a table with the recipes with highlight className
+
+    // On arrowdown interraction, if the current position is set at the end of the table:
+    // We set the currentposition at -1 ( because it will be finally set at 0 with incrementation)
+    // We remove the previous highlight className (the preview element will disapear)
+    if (evt.key === 'ArrowDown') {
+      if (navigationPosition.current === listElement.length - 1) {
+        navigationPosition.current = -1;
+        elementToRemove[0].classList.remove('highLight');
+      }
+
+      // At every interraction with arrowDown, the position is increment by +1
+      // We add the className highlight to the index of the table with the current position.
+      // We create a currentProposition variable to store the proposition's value in highLight
+      navigationPosition.current += 1;
+      listElement[navigationPosition.current].classList.add('highLight');
+
+      // If we have two element with the classname highlight , we remove the previous one
+      // (here at the index 0 cause to the different direction navigation )
+      if (elementToRemove.length === 2) {
+        elementToRemove[0].classList.remove('highLight');
+      }
+    }
+
+    // If arrowUp is the first interraction, the current position is set at -1
+    // So we set the current position to the end of the proposition's table
+    if (evt.key === 'ArrowUp') {
+      if (navigationPosition.current === -1) {
+        navigationPosition.current = listElement.length;
+      }
+
+      // If the current position is set to 0 (at the begining of the list)
+      // We set the current posittion to the end of the proposition's table
+      // And remove the previous element with className highlight
+      if (navigationPosition.current === 0) {
+        navigationPosition.current = listElement.length;
+        elementToRemove[0].classList.remove('highLight');
+      }
+
+      // At every arrowUp interraction , we decrement the current position by -1
+      // We add the className highlight to the index of the table with the current position.
+      // We create a currentProposition variable to store the proposition's value in highLight
+      navigationPosition.current -= 1;
+      listElement[navigationPosition.current].classList.add('highLight');
+
+      // If we have two element with the classname highlight , we remove the previous one
+      // (here at the index 1 cause to the different direction navigation )
+      if (elementToRemove.length === 2) {
+        elementToRemove[1].classList.remove('highLight');
+      }
+    }
+  }
+  // If we push on enter when we a element is in highlight
+  // We store the value in a reference of the current element in highlight with a useRef Hook
+  // We set the useState of the input with the useRef value
+  if (evt.key === 'Enter') {
+    const currentHighLightElement = document.getElementsByClassName('highLight');
+    currentProposition = currentHighLightElement[0].value;
+    SetSearch(currentProposition);
+    // Need to redirect on handleSubmit when its done
+  }
+}
--- src/components/Search/search.jsx
+++ src/components/Search/search.jsx
@@ -5,6 +5,9 @@ import './search.scss';
 import { React, useState, useEffect, useRef } from 'react';
 import axios from 'axios';
 
+// Function
+import keyboardNavigation from './keyboardNavigation';
+
 // Import image
 import glass from '../../assets/images/glass.png';
 import icon from '../../assets/images/icon.png';
@@ -12,11 +15,7 @@ import icon from '../../assets/images/icon.png';
 function Search() {
   const [search, SetSearch] = useState('');
   const [recipes, setRecipes] = useState([]);
-  let currentProposition = useRef(''); // We will store the value in a reference of the current element (proposition in searchBar) in highlight
-
-  // We create on object to set the propertie of position at every interraction
-  // The properties will be increment at every key push.
-  const navigationPosition = { current: -1 };
+  const currentProposition = useRef(''); // We will store the value in a reference of the current element (proposition in searchBar) in highlight
 
   // Base URL for search by cocktail category because
   // we use free access and cant have the full list for the momment
@@ -38,72 +37,6 @@ function Search() {
   // only after the first letter (with .slice )and transform in lower case. Check MDN Js function in details and this post : https://stackoverflow.com/questions/71595722/auto-capitalization-of-input-value-in-react
   };
 
-  const keyboardNavigation = (event) => {
-    if (search && search) { // We check if the input is'nt empty
-      const listElement = document.getElementsByClassName('navbar--search__input__items__list__element'); // We create a table with all propositions
-      const elementToRemove = document.getElementsByClassName('highLight'); // We create a table with the recipes with highlight className
-
-      // On arrowdown interraction, if the current position is set at the end of the table:
-      // We set the currentposition at -1 ( because it will be finally set at 0 with incrementation)
-      // We remove the previous highlight className (the preview element will disapear)
-      if (event.key === 'ArrowDown') {
-        if (navigationPosition.current === listElement.length - 1) {
-          navigationPosition.current = -1;
-          elementToRemove[0].classList.remove('highLight');
-        }
-
-        // At every interraction with arrowDown, the position is increment by +1
-        // We add the className highlight to the index of the table with the current position.
-        // We create a currentProposition variable to store the proposition's value in highLight
-        navigationPosition.current += 1;
-        listElement[navigationPosition.current].classList.add('highLight');
-
-        // If we have two element with the classname highlight , we remove the previous one
-        // (here at the index 0 cause to the different direction navigation )
-        if (elementToRemove.length === 2) {
-          elementToRemove[0].classList.remove('highLight');
-        }
-      }
-
-      // If arrowUp is the first interraction, the current position is set at -1
-      // So we set the current position to the end of the proposition's table
-      if (event.key === 'ArrowUp') {
-        if (navigationPosition.current === -1) {
-          navigationPosition.current = listElement.length;
-        }
-
-        // If the current position is set to 0 (at the begining of the list)
-        // We set the current posittion to the end of the proposition's table
-        // And remove the previous element with className highlight
-        if (navigationPosition.current === 0) {
-          navigationPosition.current = listElement.length;
-          elementToRemove[0].classList.remove('highLight');
-        }
-
-        // At every arrowUp interraction , we decrement the current position by -1
-        // We add the className highlight to the index of the table with the current position.
-        // We create a currentProposition variable to store the proposition's value in highLight
-        navigationPosition.current -= 1;
-        listElement[navigationPosition.current].classList.add('highLight');
-
-        // If we have two element with the classname highlight , we remove the previous one
-        // (here at the index 1 cause to the different direction navigation )
-        if (elementToRemove.length === 2) {
-          elementToRemove[1].classList.remove('highLight');
-        }
-      }
-    }
-    // If we push on enter when we a element is in highlight
-    // We store the value in a reference of the current element in highlight with a useRef Hook
-    // We set the useState of the input with the useRef value
-    if (event.key === 'Enter') {
-      const currentHighLightElement = document.getElementsByClassName('highLight');
-      currentProposition = currentHighLightElement[0].value;
-      SetSearch(currentProposition);
-      // Need to redirect on handleSubmit when its done
-    }
-  };
-
   return (
     <div className="navbar--search">
       <input className="navbar--search__checkbox" type="checkbox" />
@@ -119,7 +52,7 @@ function Search() {
             placeholder="Find a recipe..."
             value={search}
             onChange={(evt) => handleSearchInput(evt)}
-            onKeyDown={(evt) => keyboardNavigation(evt)}
+            onKeyDown={(evt) => keyboardNavigation(evt,  search, SetSearch, currentProposition, recipes , setRecipes)}
           />
           <button className="navbar--search__input__items__button" type="submit" label="searchBar" href="#">
             <img className="navbar--search__input__items__image" src={glass} alt="" />
