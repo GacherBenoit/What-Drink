dc6dea1a1d982d4ecb8da9034a089db48a6ce9ba
Benoit Gacher <benoitgacher@gmail.com>
2023-06-02 13:48:11 +0200

    need to see for curved line in css

--- src/components/Card/card.jsx
+++ src/components/Card/card.jsx
@@ -6,8 +6,13 @@ import './card.scss';
 
 function Card() {
   return (
-    <div className='card'>
-        <h3 className='Card--title'>Mojito</h3>
+    <div className="card">
+      <div className="card--container">
+        <div className="card--container__element">
+          <img className="card--container__element__image" src="https://www.thecocktaildb.com/images/media/drink/yqvvqs1475667388.jpg" alt="" />
+          <div className="card--container__element__image__bottom" />
+        </div>
+      </div>
     </div>
   );
 }
