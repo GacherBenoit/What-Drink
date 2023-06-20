9e58363a825dd1d73193da4592729854bbb9eedb
Benoit Gacher <benoitgacher@gmail.com>
2023-06-20 15:25:38 +0200

    slider logical ok

--- src/components/SearchResult/searchResult.jsx
+++ src/components/SearchResult/searchResult.jsx
@@ -2,20 +2,63 @@
 import './searchResult.scss';
 
 // import NPM
-import { React } from 'react';
+import { React, useEffect } from 'react';
 import PropTypes from 'prop-types';
 
 // Component
 import Card from '../Card/card';
 
+// Icons
+import Arrow from '../../assets/images/arrow.png';
+
 function SearchResult({ recipes, searchSend }) {
-  console.log(searchSend);
+  //
+  // current position in slider
+  let currentPosition = 0;
+
+  // Current card
+  // We use useEffect to initialize the 'current' class to card
+  // at every search send by user with searchSend state in parameter
+  let cards = document.getElementsByClassName('card');
+
+  useEffect(() => {
+    if (cards.length > 0) {
+      const currentCard = cards[currentPosition];
+      currentCard.classList.add('current');
+    }
+  }, [searchSend]);
+
+  // Check Position
+  const checkPosition = () => {
+    if (currentPosition === -1) {
+      currentPosition = cards.length - 1;
+    } else if (currentPosition === cards.length) {
+      currentPosition = 0;
+    }
+  };
+
+  // OnClick left
+  const handleLeftClick = () => {
+    cards[currentPosition].classList.remove('current');
+    currentPosition -= 1;
+    checkPosition();
+    cards[currentPosition].classList.add('current');
+  };
+
+  // Onclick right
+  const handleRightClick = () => {
+    cards[currentPosition].classList.remove('current');
+    currentPosition += 1;
+    checkPosition();
+    cards[currentPosition].classList.add('current');
+  };
+
   return (
     <div className="searchResult">
       <div className="searchResult--header">
         <h1 className="searchResult--header__title">WE FOUND FOR YOU</h1>
       </div>
-      <section className="searchResult--cardList">
+      <section className="searchResult--cardlist">
         { recipes.filter((recipe) => recipe.strDrink.includes(searchSend))
           .map((recipe) => (
             <Card
@@ -24,6 +67,14 @@ function SearchResult({ recipes, searchSend }) {
             />
           ))}
       </section>
+      <div className="searchResult--cardlist__arrows">
+        <button type="button" label="previous recipe" className="searchResult--cardlist__arrows__left__button" onClick={handleLeftClick}>
+          <img src={Arrow} alt="" className="searchResult--cardlist__arrows__left__button__image" />
+        </button>
+        <button type="button" label="next recipe" className="searchResult--cardlist__arrows__right__button" onClick={handleRightClick}>
+          <img src={Arrow} alt="" className="searchResult--cardlist__arrows__right__button__image" />
+        </button>
+      </div>
     </div>
   );
 }
